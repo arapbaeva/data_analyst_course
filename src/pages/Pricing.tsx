ef0c12c8bc035ef389169f28d57cc918d4d84074
arapbaeva <aiymvector@gmail.com>
2023-06-20 21:52:44 +0600

    fix bugs

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -181,14 +181,14 @@ const Heading = styled.h2`
 `;
 
 const TextWrapper = styled.span`
-	color: ${({ color }) => (color ? color : '')};
-	font-size: ${({ size }) => (size ? size : '')};
-	font-weight: ${({ weight }) => (weight ? weight : '')};
-	letter-spacing: ${({ spacing }) => (spacing ? spacing : '')};
-	padding: ${({ padding }) => (padding ? padding : '')};
-	margin: ${({ margin }) => (margin ? margin : '')};
-	margin-bottom: ${({ mb }) => (mb ? mb : '')};
-	margin-top: ${({ mt }) => (mt ? mt : '')};
+	// color: ${({ color }) => (color ? color : '')};
+	// font-size: ${({ size }) => (size ? size : '')};
+	// font-weight: ${({ weight }) => (weight ? weight : '')};
+	// letter-spacing: ${({ spacing }) => (spacing ? spacing : '')};
+	// padding: ${({ padding }) => (padding ? padding : '')};
+	// margin: ${({ margin }) => (margin ? margin : '')};
+	// margin-bottom: ${({ mb }) => (mb ? mb : '')};
+	// margin-top: ${({ mt }) => (mt ? mt : '')};
 `;
 
 const Button = styled.button`
